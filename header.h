3c9fc7c51d2a7eb0f5e7b3b00e2d64c74231bc6a
Adam <aalston9@gmail.com>
2020-07-01 00:07:15 -0400

    {Ethernet, IP, ICMP, UDP, TCP} headers

--- header.h
+++ header.h
@@ -28,9 +28,9 @@ struct icmpheader
 {
   unsigned char icmp_type;        // ICMP message type
   unsigned char icmp_code;        // Error code
-  unsigned short int icmp_chksum; //Checksum for ICMP Header and data
-  unsigned short int icmp_id;     //Used for identifying request
-  unsigned short int icmp_seq;    //Sequence number
+  unsigned short int icmp_chksum; // Checksum for ICMP Header and data
+  unsigned short int icmp_id;     // Used for identifying request
+  unsigned short int icmp_seq;    // Sequence number
 };
 
 /* UDP Header */
